c774e233370dcda73b38fff60a5d6b689272f238
dprabu17 <prabud@spritle.com>
2014-12-01 22:40:41 +0530

    node using express sample

---
+++ node-with-express-demo/README.md
@@ -0,0 +1,7 @@
+To Run
+
+1. Install express module.
+npm install express --save
+
+2. Run server file. 
+node server
---
+++ node-with-express-demo/server.js
@@ -0,0 +1,16 @@
+// define our app using express
+var express = require('express'),
+    app = express();
+
+var router = express.Router();
+router.get('/',function(req, res){
+  //res.send('hello world');
+  res.sendFile(__dirname + '/app/views/index.html');
+});
+
+app.use('/', router);
+
+var port = process.env.PORT || 3000;
+app.listen(port , function(){
+console.log('Server is running at port 3000');
+});
